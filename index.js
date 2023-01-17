0c68d921efec77d383093743292acfba2dbbb477
Nadir hussain <zeeshanahmedd0010@gmail.com>
2023-01-17 17:13:34 +0500

    setted-base

--- index.js
+++ index.js
@@ -7,16 +7,11 @@ app.use(express.static(path.join(__dirname,'public')))
 
 const port = process.env.PORT || "8000";
 
-var sampleRoute = require('./routes/sampleRoute')
-
 app.get("/", (req, res) => {
     res.status(200).send("This is basic template.");
 });
 
 //Replace with your.
-app.use("/template",sampleRoute);
-
-
 
 app.listen(port, () => {
     console.log(`Listening to requests on http://localhost:${port}`);
